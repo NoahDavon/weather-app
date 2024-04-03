8e556cd2c01202e3335d8224f933ca88945eeda3
Mohammed <MohammedE.Fahmy@outlook.com>
2024-04-03 10:05:05 +0200

    Search bar

--- weather-app/app/page.tsx
+++ weather-app/app/page.tsx
@@ -1,18 +1,24 @@
-import Image from "next/image";
+'use client'
 import WeatherCard from "./components/WeatherCard";
 import DetailsPanel from "./components/DetailsPanel";
+import { SearchIcon } from "@chakra-ui/icons";
+import { Input } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <div className=" bg-white p-2 ">
-      <div className="flex flex-col gap-2 w-full overflow-y-scroll">
+    <div className=" bg-white p-2 flex flex-col gap-2 h-[95vh] ">
+      <div className="flex bg-slate-50 rounded-full w-full p-4 items-center">
+        <SearchIcon/>
+        <Input placeholder="Search for city..." border={"none"} _active={{border: 0}}/>
+      </div>
+      <DetailsPanel/>
+      <div className="flex flex-col gap-2 w-full overflow-y-scroll basis-0 flex-grow">
         <WeatherCard/>
         <WeatherCard/>
         <WeatherCard/>
         <WeatherCard/>
         <WeatherCard/>
       </div>
-      <DetailsPanel/>
     </div>
   );
 }
