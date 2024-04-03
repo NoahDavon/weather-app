642163ca8dda1b876eba8ceeb889053c860408fa
Mohammed <MohammedE.Fahmy@outlook.com>
2024-04-03 09:54:34 +0200

    Fix details

--- weather-app/app/components/DetailsPanel.tsx
+++ weather-app/app/components/DetailsPanel.tsx
@@ -7,21 +7,21 @@ type Props = {}
 export default function DetailsPanel({}: Props) {
   return (
     <div className='flex bg-blue-200 rounded-md items-center overflow-x-scroll p-4 gap-4'>
-        <div className='flex flex-col'>
+        <div className='flex flex-col min-w-28'>
             <Text>Thu, Mar 7</Text>
             <Text>London, UK</Text>
         </div>
-        <Divider orientation="vertical"/>
-        <div className='flex items-center'>
+        <Divider orientation="vertical" height={"40px"}/>
+        <div className='flex items-center min-w-40'>
             <Image src={WMO[0].day.image} className=' -m-4'/>
             <div className='flex flex-col'>
-                <Text>{WMO[0].day.description}</Text>
+                <Text overflowWrap={"normal"}>{WMO[99].day.description}</Text>
                 <Text>{`24\u00b0C`}</Text>
             </div>
             
         </div>
-        <Divider orientation="vertical"/>
-        <div className='flex flex-col'>
+        <Divider orientation="vertical" height={"40px"}/>
+        <div className='flex flex-col min-w-32'>
             <Text>Precipitation: 1%</Text>
             <Text>Wind speed: 57km/h</Text>
         </div>
--- weather-app/app/page.tsx
+++ weather-app/app/page.tsx
@@ -4,8 +4,8 @@ import DetailsPanel from "./components/DetailsPanel";
 
 export default function Home() {
   return (
-    <div>
-      <div className=" bg-white p-2 flex flex-col gap-2 w-full overflow-y-scroll">
+    <div className=" bg-white p-2 ">
+      <div className="flex flex-col gap-2 w-full overflow-y-scroll">
         <WeatherCard/>
         <WeatherCard/>
         <WeatherCard/>
